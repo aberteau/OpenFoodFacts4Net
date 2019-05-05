00284b212e9ce3bf2c18d26f217a5bf581d2e1ac
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 12:42:05 +0200

    Ajout projet TestConsoleApp

---
+++ src/TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenFoodFacts4Net.TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
