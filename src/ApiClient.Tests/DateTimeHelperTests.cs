86286463ca0c5bb641c2c8734d41183c9f1a9d4b
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 21:57:09 +0200

    Refactoring DateTimeHelperTests

--- src/ApiClient.Tests/DateTimeHelperTests.cs
+++ src/ApiClient.Tests/DateTimeHelperTests.cs
@@ -8,7 +8,7 @@ namespace OpenFoodFacts4Net.ApiClient.Tests
         [Theory]
         [InlineData(1556280105, 2019, 4, 26, 12, 1, 45)]
         [InlineData(1338046925, 2012, 5, 26, 15, 42, 5)]
-        public void UnixTimestampToDateTimeTest01(Int64 timestamp, Int32 year, Int32 month, Int32 day, Int32 hour, Int32 minute, Int32 second)
+        public void Should_Convert_UnixTimestamp_ToDateTime(Int64 timestamp, Int32 year, Int32 month, Int32 day, Int32 hour, Int32 minute, Int32 second)
         {
             DateTime expectedDateTime = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Utc);
             
