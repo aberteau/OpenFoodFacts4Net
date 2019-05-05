927def16c0b75c4e9e00b7b45254e2852e83d6e6
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 14:05:23 +0200

    Ajout ProductStringConverter dans ApiClient

---
+++ src/ApiClient/Converters/ProductStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenFoodFacts4Net.ApiClient.Converters
+{
+    public class ProductStringConverter
+        : JsonConverter<String>
+    {
+        public override void WriteJson(JsonWriter writer, string value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ReadJson(JsonReader reader, Type objectType, string existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            string str = (string)reader.Value;
+            //StringEscapeUtils.unescapeHtml4(value).replace("\\'", "'").replace("&quot", "'");
+            return str;
+        }
+    }
+}
