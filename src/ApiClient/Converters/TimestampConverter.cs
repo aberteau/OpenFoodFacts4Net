4316f9a18098820da482c9fa8ca5ec1d391d7559
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 14:05:46 +0200

    Ajout TimestampConverter dans ApiClient

---
+++ src/ApiClient/Converters/TimestampConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenFoodFacts4Net.ApiClient.Converters
+{
+    public class TimestampConverter
+        : JsonConverter<DateTime>
+    {
+        public override void WriteJson(JsonWriter writer, DateTime value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime ReadJson(JsonReader reader, Type objectType, DateTime existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            long intValue = Convert.ToInt64(reader.Value);
+            DateTime dateTime = DateTimeHelper.UnixTimestampToDateTime(intValue);
+            return dateTime;
+        }
+    }
+}
