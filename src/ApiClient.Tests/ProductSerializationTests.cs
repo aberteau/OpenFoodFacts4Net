33c6d3eef93d0af8233232d5df99242a4d2f2d6e
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 22:04:13 +0200

    fixup bc5a21e

--- src/ApiClient.Tests/ProductSerializationTests.cs
+++ src/ApiClient.Tests/ProductSerializationTests.cs
@@ -18,14 +18,5 @@ namespace OpenFoodFacts4Net.ApiClient.Tests
             Assert.Equal("Pain de mie complet", product.GenericName);
             Assert.NotNull(product.NutrientLevels);
         }
-
-        [Fact]
-        public void Should_Deserialize_Product_3029330003533_Partial01()
-        {
-            string json = DataSetHelper.ReadFileContent("Product.3029330003533-Partial01.json");
-            Product product = JsonConvert.DeserializeObject<Product>(json);
-
-            Assert.NotNull(product.NutrientLevels);
-        }
     }
 }
