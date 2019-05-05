5b6f346975609374f1584140eab0c266167ff937
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-05 13:02:12 +0200

    Ajout DateTimeHelper dans ApiClient

---
+++ src/ApiClient/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenFoodFacts4Net.ApiClient
+{
+    public class DateTimeHelper
+    {
+        public static DateTime UnixTimestampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
+            return dtDateTime;
+        }
+    }
+}
