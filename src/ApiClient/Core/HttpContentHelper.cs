a386ab5f4e085bda94468687acdab49e72195094
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 21:41:25 +0200

    Ajout HttpContentHelper dans ApiClient

---
+++ src/ApiClient/Core/HttpContentHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenFoodFacts4Net.ApiClient.Core
+{
+    public class HttpContentHelper
+    {
+        public static HttpContent ToJsonContent<T>(T contentObj)
+        {
+            string jsonObject = contentObj == null ? null : JsonConvert.SerializeObject(contentObj);
+            HttpContent content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+            return content;
+        }
+    }
+}
