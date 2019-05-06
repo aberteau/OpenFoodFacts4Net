df629f2fa78705932e4b6843dc78862dc19e150e
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 21:42:19 +0200

    Ajout HttpContentExtensions

---
+++ src/ApiClient/Core/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OpenFoodFacts4Net.ApiClient.Core.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        private static T DeserializeObject<T>(string contentString)
+            where T : class
+        {
+            if (String.IsNullOrWhiteSpace(contentString))
+                return null;
+
+            T obj = JsonConvert.DeserializeObject<T>(contentString);
+            return obj;
+        }
+
+        public static async Task<T> ReadAsAsync<T>(this HttpContent httpContent)
+            where T : class
+        {
+            string contentString = await httpContent.ReadAsStringAsync();
+            T obj = DeserializeObject<T>(contentString);
+            return obj;
+        }
+    }
+}
