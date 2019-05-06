bfd6653861188e20acad73ff895cdfa4e96f6642
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 21:44:48 +0200

    Utilisation méthode d'extension GetAsync générique

--- src/ApiClient/Extensions/HttpClientExtensions.cs
+++ src/ApiClient/Extensions/HttpClientExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using OpenFoodFacts4Net.ApiClient.Core;
+using OpenFoodFacts4Net.ApiClient.Core.Extensions;
 using OpenFoodFacts4Net.Json.Data;
 
 namespace OpenFoodFacts4Net.ApiClient.Extensions
@@ -11,11 +11,8 @@ namespace OpenFoodFacts4Net.ApiClient.Extensions
     {
         public static async Task<GetProductResponse> GetProductAsync(this HttpClient httpClient, String barcode)
         {
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"/api/v0/product/{barcode}.json");
-            httpResponseMessage.EnsureSuccessStatusCode();
-            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
-            GetProductResponse response = JsonConvert.DeserializeObject<GetProductResponse>(responseBody);
-            return response;
+            IResponse<GetProductResponse> response = await httpClient.GetAsync<GetProductResponse>($"/api/v0/product/{barcode}.json");
+            return response.Data;
         }
     }
 }
