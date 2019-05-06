2ab06e30acb1471ac456fe7bfeb77476355fa1e0
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 12:43:17 +0200

    Ajout StringBuilderExtensions

---
+++ src/ApiClient/StringBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenFoodFacts4Net.ApiClient
+{
+    static class StringBuilderExtensions
+    {
+        public static bool IsEmpty(this StringBuilder stringBuilder)
+        {
+            return (stringBuilder.Length == 0);
+        }
+
+        /// <summary>
+        /// Ajoute la chaîne <paramref name="str"/> au <paramref name="stringBuilder"/> si <paramref name="stringBuilder"/> non vide
+        /// </summary>
+        /// <param name="stringBuilder">StringBuilder</param>
+        /// <param name="str">Chaîne à ajouter au <paramref name="stringBuilder"/></param>
+        /// <returns>StringBuilder</returns>
+        public static StringBuilder AppendIfNotEmpty(this StringBuilder stringBuilder, String str)
+        {
+            if (!stringBuilder.IsEmpty())
+                stringBuilder.Append(str);
+
+            return stringBuilder;
+        }
+
+        /// <summary>
+        /// Ajoute un espace au <paramref name="stringBuilder"/> si <paramref name="stringBuilder"/> non vide
+        /// </summary>
+        /// <param name="stringBuilder">StringBuilder</param>
+        /// <returns>StringBuilder</returns>
+        public static StringBuilder AppendSpaceIfNotEmpty(this StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendIfNotEmpty(" ");
+            return stringBuilder;
+        }
+    }
+}
