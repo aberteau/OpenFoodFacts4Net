e57c6d6def5a62fe48b5ee71dc23ace8ec07fc1f
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 18:58:25 +0200

    Ajout Client dans projet ApiClient

---
+++ src/ApiClient.Tests/ClientTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenFoodFacts4Net.Json.Data;
+using Xunit;
+
+namespace OpenFoodFacts4Net.ApiClient.Tests
+{
+    public class ClientTests
+    {
+        [Fact]
+        public async Task Should_GetProductAsync_3029330003533()
+        {
+            string barcode = "3029330003533";
+            String userAgent = UserAgentHelper.GetUserAgent("OpenFoodFacts4Net.ApiClient.Tests", ".Net Platform", "0.1",  null);
+
+            Client client = new Client(Constants.BaseUrl, userAgent);
+            GetProductResponse productResponse = await client.GetProductAsync(barcode);
+
+            Assert.Equal("Pain de mie complet", productResponse.Product.GenericName);
+        }
+    }
+}
---
+++ src/ApiClient/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OpenFoodFacts4Net.ApiClient.Extensions;
+using OpenFoodFacts4Net.Json.Data;
+
+namespace OpenFoodFacts4Net.ApiClient
+{
+    public class Client
+    {
+        private readonly HttpClient _httpClient;
+
+        public Client(string baseUrl, String userAgent)
+        {
+            _httpClient = HttpClientHelper.Create(baseUrl, userAgent);
+        }
+
+        public async Task<GetProductResponse> GetProductAsync(String barcode)
+        {
+            GetProductResponse response = await _httpClient.GetProductAsync(barcode);
+            return response;
+        }
+    }
+}
