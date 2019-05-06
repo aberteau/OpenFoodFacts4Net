16cfee3dbfb1154506d35d1bc7025a3cb3376f25
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 18:57:04 +0200

    Ajout HttpClientExtensions dans projet ApiClient

---
+++ src/ApiClient.Tests/HttpClientExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OpenFoodFacts4Net.ApiClient.Extensions;
+using OpenFoodFacts4Net.Json.Data;
+using Xunit;
+
+namespace OpenFoodFacts4Net.ApiClient.Tests
+{
+    public class HttpClientExtensionsTests
+    {
+        [Fact]
+        public async Task Should_GetProductAsync_3029330003533()
+        {
+            string barcode = "3029330003533";
+            String userAgent = UserAgentHelper.GetUserAgent("OpenFoodFacts4Net.ApiClient.Tests", ".Net Platform", "0.1",  null);
+            Uri baseAddress = new Uri(Constants.BaseUrl);
+            HttpClient httpClient = HttpClientHelper.Create(baseAddress, userAgent);
+            GetProductResponse productResponse = await httpClient.GetProductAsync(barcode);
+
+            Assert.Equal("Pain de mie complet", productResponse.Product.GenericName);
+        }
+    }
+}
---
+++ src/ApiClient/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OpenFoodFacts4Net.Json.Data;
+using RestSharp;
+
+namespace OpenFoodFacts4Net.ApiClient.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<GetProductResponse> GetProductAsync(this HttpClient httpClient, String barcode)
+        {
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"/api/v0/product/{barcode}.json");
+            httpResponseMessage.EnsureSuccessStatusCode();
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            GetProductResponse response = JsonConvert.DeserializeObject<GetProductResponse>(responseBody);
+            return response;
+        }
+    }
+}
