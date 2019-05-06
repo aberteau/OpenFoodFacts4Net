0a200b7cebf78cafe2eb46e2c8e1622cef8043b3
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-06 21:42:34 +0200

    Ajout HttpResponseMessageExtensions

---
+++ src/ApiClient/Core/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenFoodFacts4Net.ApiClient.Core.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<T> ReadContentAsAsync<T>(this HttpResponseMessage response)
+            where T : class
+        {
+            T obj = await response.Content.ReadAsAsync<T>();
+            return obj;
+        }
+    }
+}
