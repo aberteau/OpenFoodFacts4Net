7e51d2c3587c7fa1775dc131c887baaf1eaf18e8
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-05-18 14:45:28 +0200

    Ajout génération package NuGet OpenFoodFacts4Net.Csv

--- createPackages.bat
+++ createPackages.bat
@@ -1,2 +1,3 @@
 dotnet pack "src\Json" --configuration=Release --output "..\..\packages"
 dotnet pack "src\ApiClient" --configuration=Release --output "..\..\packages"
+dotnet pack "src\Csv" --configuration=Release --output "..\..\packages"
