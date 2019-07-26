9ad4127bd16ba07855d86d5e0a42ea46571afb50
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-07-26 15:48:31 +0200

    Ajout génération package NuGet OpenFoodFacts4Net.Taxonomy.Json

--- createPackages.bat
+++ createPackages.bat
@@ -1,3 +1,4 @@
 dotnet pack "src\Json" --configuration=Release --output "..\..\packages"
 dotnet pack "src\ApiClient" --configuration=Release --output "..\..\packages"
 dotnet pack "src\Csv" --configuration=Release --output "..\..\packages"
+dotnet pack "src\Taxonomy.Json" --configuration=Release --output "..\..\packages"
