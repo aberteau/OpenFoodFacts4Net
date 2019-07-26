8389c8c02c20b47017175ab49baeb68fe8c281aa
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-07-26 15:40:20 +0200

    Ajout du projet Taxonomy.Json

---
+++ src/Taxonomy.Json/Data/Taxon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenFoodFacts4Net.Taxonomy.Json.Data
+{
+    public class Taxon
+    {
+        [JsonProperty("name")]
+        public IDictionary<String, String> Name { get; set; }
+
+        [JsonProperty("parents")]
+        public IEnumerable<String> Parents { get; set; }
+
+        [JsonProperty("children")]
+        public IEnumerable<String> Children { get; set; }
+
+        [JsonProperty("country")]
+        public IDictionary<String, String> Country { get; set; }
+
+        [JsonProperty("region")]
+        public IDictionary<String, String> Region { get; set; }
+
+        [JsonProperty("instanceof")]
+        public IDictionary<String, String> InstanceOf { get; set; }
+
+        [JsonProperty("wikidata")]
+        public IDictionary<String, String> Wikidata { get; set; }
+    }
+}
---
+++ src/Taxonomy.Json/TaxonomySerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using OpenFoodFacts4Net.Taxonomy.Json.Data;
+
+namespace OpenFoodFacts4Net.Taxonomy.Json
+{
+    public class TaxonomySerializer
+    {
+        public static IDictionary<String, Taxon> Deserialize(String json)
+        {
+            IDictionary<String, Taxon> taxonomyData = JsonConvert.DeserializeObject<IDictionary<String, Taxon>>(json);
+            return taxonomyData;
+        }
+    }
+}
