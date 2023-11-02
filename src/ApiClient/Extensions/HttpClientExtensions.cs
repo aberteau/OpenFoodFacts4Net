2e338afeb71e801a453bf17be89026a0c6c92a1c
Amael BERTEAU <amael.berteau@tech-easy.fr>
2023-11-03 00:45:40 +0100

    [ApiClient] Using API v2 in method GetProductAsync

--- src/ApiClient/Extensions/HttpClientExtensions.cs
+++ src/ApiClient/Extensions/HttpClientExtensions.cs
@@ -11,7 +11,7 @@ namespace OpenFoodFacts4Net.ApiClient.Extensions
     {
         public static async Task<GetProductResponse> GetProductAsync(this HttpClient httpClient, String barcode)
         {
-            string requestUri = $"/api/v0/product/{barcode}.json";
+            string requestUri = $"/api/v2/product/{barcode}.json";
             IResponse<GetProductResponse> response = await httpClient.GetAsync<GetProductResponse>(requestUri);
             return response.Data;
         }
