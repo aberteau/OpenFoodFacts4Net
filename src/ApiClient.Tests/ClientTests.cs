faea75289ab967505bfec519e4ccc7849201c4b1
Amael BERTEAU <amael.berteau@tech-easy.fr>
2023-11-03 00:50:11 +0100

    [ApiClient.Tests] Updating tests in ClientTests

--- src/ApiClient.Tests/ClientTests.cs
+++ src/ApiClient.Tests/ClientTests.cs
@@ -9,15 +9,15 @@ namespace OpenFoodFacts4Net.ApiClient.Tests
     public class ClientTests
     {
         [Fact]
-        public async Task Should_GetProductAsync_3029330003533()
+        public async Task Should_GetProductAsync_3017620422003()
         {
-            string barcode = "3029330003533";
+            string barcode = "3017620422003";
             String userAgent = UserAgentHelper.GetUserAgent("OpenFoodFacts4Net.ApiClient.Tests", ".Net Platform", "0.1",  null);
 
             Client client = new Client(Constants.BaseUrl, userAgent);
             GetProductResponse productResponse = await client.GetProductAsync(barcode);
 
-            productResponse.Product.GenericName.Should().Be("Pain de mie complet");
+            productResponse.Product.GenericName.Should().Be("Pâte à tartiner aux noisettes et au cacao");
         }
     }
 }
