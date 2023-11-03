2e6f7a2f42c40fe45dd3287910366cd503f8c4c0
Amael BERTEAU <amael.berteau@tech-easy.fr>
2023-11-03 01:38:13 +0100

    [Json] Sorting properties alphabetically in NutrientLevels

--- src/Json/Data/NutrientLevels.cs
+++ src/Json/Data/NutrientLevels.cs
@@ -5,20 +5,20 @@ namespace OpenFoodFacts4Net.Json.Data
 {
     public class NutrientLevels
     {
-        [JsonProperty("salt")]
-        [JsonConverter(typeof(StringEnumConverter))]
-        public NutrientLevel Salt { get; set; }
-
         [JsonProperty("fat")]
         [JsonConverter(typeof(StringEnumConverter))]
         public NutrientLevel Fat { get; set; }
 
-        [JsonProperty("sugars")]
+        [JsonProperty("salt")]
         [JsonConverter(typeof(StringEnumConverter))]
-        public NutrientLevel Sugars { get; set; }
+        public NutrientLevel Salt { get; set; }
 
         [JsonProperty("saturated-fat")]
         [JsonConverter(typeof(StringEnumConverter))]
         public NutrientLevel SaturatedFat { get; set; }
+
+        [JsonProperty("sugars")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public NutrientLevel Sugars { get; set; }
     }
 }
